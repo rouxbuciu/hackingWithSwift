9d1278daaae0fe431ab3573ff8d7f92e65a73cd9
roux buciu <roux_buciu@symantec.com>
2019-05-16 18:13:33 -0400

    Git add project1

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Roux Buciu on 2019-05-16.
+//  Copyright © 2019 Roux Buciu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
