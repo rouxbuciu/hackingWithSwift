f4bae51f132bff6b8706ca0864c72bd72d031ddc
roux buciu <roux_buciu@symantec.com>
2019-08-06 14:07:13 -0400

    Add project 18

---
+++ Project18 - Debugging/Project18 - Debugging/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18 - Debugging
+//
+//  Created by Roux Buciu on 2019-08-01.
+//  Copyright © 2019 Roux Buciu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
